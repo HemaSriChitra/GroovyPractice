620e2c072bec4f73e9dd2a779e5760b5816c707d
HemaSriChitra <148052127+HemaSriChitra@users.noreply.github.com>
2023-10-17 10:58:32 +0530

    commit
    Largest Number Code

---
+++ Basics/LargeNum.groovy
@@ -0,0 +1,13 @@
+class LargeNum{
+
+    static void main(String[] args){
+
+        def num=[76,45,22,34,98];
+
+        def largestNumber=num.max();
+
+        println("The largest number is  "+largestNumber);
+
+    }
+
+}
