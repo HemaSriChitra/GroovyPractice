b51c4265f3faad31c409d81cccccaa39d35732e3
HemaSriChitra <148052127+HemaSriChitra@users.noreply.github.com>
2023-10-17 15:15:55 +0530

    commit
    Concatination code

---
+++ Basics/Concat.groovy
@@ -0,0 +1,19 @@
+class Concatination{
+
+    static void main(String[] args){
+
+        //defining a String
+
+        String name1='Hema';
+
+        String name2='Pasumpon';
+
+        //concating the two strings
+
+        String Concat=name1+name2;
+        //print the output
+        println("The Concated String  :" +Concat) ;   
+
+        }
+
+}
