250c79112a3033f299b6f4bbd249500d9f2ad4ca
HemaSriChitra <148052127+HemaSriChitra@users.noreply.github.com>
2023-10-17 11:12:42 +0530

    add command line

--- Basics/SimpleInterest.groovy
+++ Basics/SimpleInterest.groovy
@@ -2,8 +2,11 @@ class SimpleInterest
 {
     static void main(String[] args)
     {
+ //declare data type as float
 float p=10000,r=3.87,t=5;
+//formula to calculate simple interest
 float SI=(p*t*r)/100
+//print statement for print the calue of SI
 print(SI)
     }
 }
