8b908a8586c937f40eb5bc6f8b699653b166bba6
HemaSriChitra <148052127+HemaSriChitra@users.noreply.github.com>
2023-10-18 14:48:30 +0530

    commit

---
+++ Basics/MapSet.groovy
@@ -0,0 +1,66 @@
+class MapSet{
+
+    static void main(String[] args){
+
+ //set  creation  
+
+Set games = ["cricket", "throw ball", "tennis"]
+
+ 
+
+//map creation
+
+def gamesMap = [
+
+    "a" : "cricket",
+
+    "b" : "throw ball",
+
+    "c" : "tennis"
+
+]
+
+println("Original Set: $gamesSet")
+
+println("Original Map: $gamesMap")
+
+// definition in a Set
+
+def found = gamesSet.contains("cricket")
+
+println("'cricket' is found in the Set? $found")
+
+// definition in a Map
+
+def foundInMap = gamesMap["b"]
+
+println("Value for key 'b' in Map: $foundInMap")
+
+// Adding elements to a Set
+
+gamesSet << "date"
+
+println("Set after adding 'date': $gamesSet")
+
+// Adding elements to a Map
+
+SetMap["d"] = "date"
+
+println("Map after adding 'date': $gamesMap")
+
+ 
+// Removing elements from a Set
+
+gamesSet.remove("throw ball")
+
+println("Set after removing 'banana': $gamesSet")
+
+// Removing elements from a Map
+
+gamesMap.remove("c")
+
+println("Map after removing key 'c': $gamesMap")
+
+  }
+
+}
